482e036201592b59363ce2d57fc70741fe0713a6
Jesus Lopez <jalopezvillatoro@gmail.com>
2018-01-10 17:23:41 -0600

    cambio

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,53 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <iframe width="560" height="315" src="https://emb.d.tube/#!/jlopvi/q7fknqtp" frameborder="0" allowfullscreen></iframe>
-  </div>
+<template lang="pug">
+  #app
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(
+            v-model="searchQuery",
+            type="text",
+            placeholder="Buscar canciones"
+          )
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times;
+
+      .container
+        .columns
+          .column(v-for="t in tracks") Cancion: {{ t.name }} | Artista: {{ t.artist }}
+        .columns
+          .column {{ searchMessage }}
 </template>
 
 <script>
+const tracks = [
+  { name: 'Muchacha', artist: 'Luis Alberto Spinetta' },
+  { name: 'Hoy aca en el baile', artist: 'El papo' },
+  { name: 'I was mande for loving you', artist: 'Kiss' }
+]
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Hola vue!'
+      searchQuery: '',
+      tracks: []
+    }
+  },
+
+  computed: {
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
+    }
+  },
+
+  methods: {
+    search () {
+      this.tracks = tracks
     }
   }
 }
 </script>
 
 <style lang="scss">
+  @import './scss/main.scss';
 
 </style>
